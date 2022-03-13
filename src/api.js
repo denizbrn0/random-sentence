3668dfde4e9e2cf8810fee93316c70be5d64a3b1
Deniz Baran <denizbaran128@gmail.com>
2022-03-13 21:47:27 +0300

    a

--- src/api.js
+++ src/api.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-const routers = require("./src/router/routers.js");
+const routers = require("./router/routers.js");
 const path = require("path");
 const bodyParser = require("body-parser");
-require("./src/db/dbConnect.js");
+require("./db/dbConnect.js");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", routers);
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/src/views"));
+app.set("views", path.join(__dirname, "/views"));
 
 app.listen(process.env.PORT);
